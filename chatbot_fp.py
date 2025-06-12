ae811d692f70b4c8bd4ea31fad58760c0f99cc70
antoniojasweb <65303510+antoniojasweb@users.noreply.github.com>
2025-06-12 18:51:12 +0200

    Add files via upload

--- chatbot_fp.py
+++ chatbot_fp.py
@@ -351,7 +351,7 @@ with col1:
     st.image(image, width=200)
 with col2:
     #st.title("Chatbot de Ciclos Formativos")
-    st.metrica("Chatbot de Ciclos Formativos", value = 500)
+    st.metric("Chatbot de Ciclos Formativos", value = 500)
 
 # Inicializar el estado de la sesión si no existe
 if "chat_history" not in st.session_state:
