7512d1cbe1b2d0b3cbf1533d8aad959087adb72a
antoniojasweb <65303510+antoniojasweb@users.noreply.github.com>
2025-06-12 18:26:30 +0200

    Add files via upload

--- chatbot_fp.py
+++ chatbot_fp.py
@@ -290,8 +290,8 @@ def ask_rag_model(query: str, index, corpus: list, model: SentenceTransformer, d
     st.session_state.chat_history.append({"role": "assistant", "content": f"Buscando información relacionada con '{query}'..."})
 
     # Muestra los documentos recuperados para depuración o información al usuario
-    with st.expander("Ver información recuperada: " + str(top_k) + " opciones más relevantes"):
-        st.write(retrieved_docs_df[['Nombre Ciclo', 'Municipio', 'Provincia', 'Grado', 'Familia Profesional']])
+    # with st.expander("Ver información recuperada: " + str(top_k) + " opciones más relevantes"):
+    #     st.write(retrieved_docs_df[['Nombre Ciclo', 'Municipio', 'Provincia', 'Grado', 'Familia Profesional']])
 
     #st.write("Los datos encontrados a tu pregunta son:")
     #st.write(context)
