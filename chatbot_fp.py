e14c8e654ade34a59ec681ddbc1fc6dce6259e54
antoniojasweb <65303510+antoniojasweb@users.noreply.github.com>
2025-06-12 19:04:18 +0200

    Add files via upload

--- chatbot_fp.py
+++ chatbot_fp.py
@@ -350,7 +350,7 @@ col1, col2 = st.columns([1,2])
 with col1:
     st.image(image, width=200)
 with col2:
-    st.title("Ciclos Formativos")
+    st.title("Ciclos Formativos en Extremadura")
 
 # Inicializar el estado de la sesión si no existe
 if "chat_history" not in st.session_state:
